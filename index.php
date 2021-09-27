4e2f3c01e47630a4292ee7529ae2283b2aa99fd4
Timofey <Tim.S.@MacBook-Pro-Timofey.local>
2021-09-28 00:27:15 +0300

    website crieted

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
+</head>
+<body>
+<h1>Hello evorybadii yo</h1>
+<h2><a href="http://localhost:8888/obuchalka111/zayapisya/">na hoi</a></h2>
+<?php $stroka = "12345";
+echo "<h1>" . $stroka . "</h1>"?>
+    
+</body>
+</html>
---
+++ page-zayapisya.php
@@ -0,0 +1,2 @@
+<h1>stranitsa zayi-pisi</h1>
+<h2><a href="http://localhost:8888/obuchalka111/">nazaaad bleat</a></h2>
